c93071abf2750475560aea5dd14f3fbdf4c4bc45
Pratik Saxena <pratikbsp@yahoo.com>
2016-08-23 22:47:15 +0530

    first commit

---
+++ Camniture/app/src/main/java/com/application/camniture/CamActivity.java
@@ -0,0 +1,13 @@
+package com.application.camniture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CamActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam);
+    }
+}
