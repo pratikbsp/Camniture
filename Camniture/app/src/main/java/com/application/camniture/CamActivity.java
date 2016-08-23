7e9c2e2d51fc8340c2d4ed3bfca9a68181b7ef8a
Pratik Saxena <pratikbsp@yahoo.com>
2016-08-23 23:47:51 +0530

    added camera

--- Camniture/app/src/main/java/com/application/camniture/CamActivity.java
+++ Camniture/app/src/main/java/com/application/camniture/CamActivity.java
@@ -1,5 +1,6 @@
 package com.application.camniture;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,7 @@ public class CamActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cam);
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 0);
     }
 }
